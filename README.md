ceb643facd5c49f4bcb3871c9dd75d6fdf1005dc
wi1ls <277797382@qq.com>
2020-12-08 11:19:57 +0800

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# FlutterBoot
+
+## 该项目是为了Flutter的学习和记录
---
+++ android/app/src/main/kotlin/com/wi1ls/FlutterBoot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wi1ls.FlutterBoot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
